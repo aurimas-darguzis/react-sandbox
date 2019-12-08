78a229c1b089abb5a384300e986ab0afe4077e19
aurimas-darguzis <aurimas@darguzis.com>
2019-12-08 19:22:10 +0000

    implement infinite scroll

---
+++ src/features/Hackernews/fixtures/index.js
@@ -0,0 +1,17 @@
+export const singularStory = {
+  by: "Kyle Simpson",
+  id: 1,
+  time: 1454343578,
+  title: "Functional Javascript",
+  url: "https://somebook.com"
+};
+
+export const storyIds = [1];
+
+export const emptySingularStory = {
+  by: undefined,
+  id: undefined,
+  time: undefined,
+  title: undefined,
+  url: undefined
+};
